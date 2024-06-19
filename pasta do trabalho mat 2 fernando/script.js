626eb9f682ad5ec5448663caf967d6a18ef15fd1
fernandorudnikmadeira <128594298+fernandorudnikmadeira@users.noreply.github.com>
2024-06-19 12:03:31 -0300

    Add files via upload

---
+++ pasta do trabalho mat 2 fernando/script.js
@@ -0,0 +1,4 @@
+const Caixaprincipal =document.querySelector(".Caixa-principal");
+const Caixapergunta =document.querySelector(".Caixa-pergunta");
+const Caixaalternativa =document.querySelector(".Caixa-alternativa");
+const Caixaresposta =document.querySelector(".Caixa-resposta");
