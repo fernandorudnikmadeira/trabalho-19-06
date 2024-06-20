9f405f5662821bf408b4126f6b388b087d3faf2f
fernandorudnikmadeira <128594298+fernandorudnikmadeira@users.noreply.github.com>
2024-06-20 07:57:58 -0300

    Add files via upload

---
+++ trabalho-19-06-main/pasta do trabalho mat 2 fernando/script.js
@@ -0,0 +1,24 @@
+const Caixaprincipal =document.querySelector(".Caixa-principal");
+const Caixapergunta =document.querySelector(".Caixa-pergunta");
+const Caixaalternativa =document.querySelector(".Caixa-alternativa");
+const Caixaresposta =document.querySelector(".Caixa-resposta");
+const TextoResultado =document.querySelector(".Texto-resultado");
+
+const perguntas = [
+   {
+    enunciado: "O que é a trigonometria?", 
+    alternativas: [
+       "É o estudo das relações entre ângulos e lados e algumas extensões dessas relações nos triângulo retângulo.;",
+    "alternativa 2"
+      ]
+},
+{
+      enunciado: "Aonde você pode utilizar a trigonometria? ", 
+      alternativas: [
+      "alternativa 1",
+      "alternativa 2"
+        
+    ]
+}
+
+]
