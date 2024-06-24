a0cbdc582b8664237d9f06cfb39592042066efc4
fernandorudnikmadeira <128594298+fernandorudnikmadeira@users.noreply.github.com>
2024-06-24 11:13:17 -0300

    Add files via upload

--- trabalho-19-06-main/pasta do trabalho mat 2 fernando/script.js
+++ trabalho-19-06-main/pasta do trabalho mat 2 fernando/script.js
@@ -1,24 +1,47 @@
-const Caixaprincipal =document.querySelector(".Caixa-principal");
-const Caixapergunta =document.querySelector(".Caixa-pergunta");
-const Caixaalternativa =document.querySelector(".Caixa-alternativa");
-const Caixaresposta =document.querySelector(".Caixa-resposta");
-const TextoResultado =document.querySelector(".Texto-resultado");
+const Caixaprincipal = document.querySelector(".Caixa-principal");
+const Caixapergunta = document.querySelector(".Caixa-pergunta");
+const Caixaalternativa = document.querySelector(".Caixa-alternativa");
+const Caixaresposta = document.querySelector(".Caixa-resposta");
+const Caixarusultado = document.querySelector(".Caixa-resultado");
 
 const perguntas = [
-   {
-    enunciado: "O que é a trigonometria?", 
-    alternativas: [
-       "É o estudo das relações entre ângulos e lados e algumas extensões dessas relações nos triângulo retângulo.;",
-    "alternativa 2"
-      ]
-},
-{
-      enunciado: "Aonde você pode utilizar a trigonometria? ", 
-      alternativas: [
-      "alternativa 1",
-      "alternativa 2"
-        
-    ]
+    {
+        enunciado: "pergunta 1",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    },
+    {
+        enunciado: "pergunta 2", 
+        alternativas: [
+            "alternativa 1",
+        "alternativa 2"
+       ]
+    },
+    {
+        enunciado: "pergunta 3", 
+        alternativas: [
+            "alternativa 1",
+        "alternativa 2"
+       ]
+    },
+    {
+        enunciado: "pergunta 4", 
+        alternativas: [
+            "alternativa 1",
+        "alternativa 2"
+       ]
+    }
+
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    Caixapergunta.textContent = perguntaAtual.enunciado;
 }
 
-]
+mostraPergunta();
